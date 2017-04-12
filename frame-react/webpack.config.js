8acfb3b73be9eccb7b99f3bdf9a55d7d98b71f89
shenqingling <sql370@qq.com>
2017-04-12 23:28:17 +0800

    1. 添加项目启动自动打开浏览器插件 2. 将react/react-dom/react-router作为外部资源引入

--- frame-react/webpack.config.js
+++ frame-react/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const WebpackBrowserPlugin = require('webpack-browser-plugin');
 
 module.exports = {
     entry: [
         './entry.js',
     ],
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     module: {
@@ -18,5 +20,13 @@ module.exports = {
         // publicPath: path.resolve(__dirname, '/dist'),
         // contentBase: ''
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'react-router': 'ReactRouter',
+    },
+    plugins: [
+        new WebpackBrowserPlugin()
+    ],
 }
