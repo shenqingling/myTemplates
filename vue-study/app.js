3487f8b2a4ce51118a746d9de1639506fb665e2b
沈庆玲 <sql370@qq.com>
2017-06-23 17:52:42 +0800

    vue start

---
+++ vue-study/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import './index.js';
+// import { Index } from './Index.vue';
+
+var app = new Vue({
+    el: '#app',
+    template: '<index />',
+// component: Index
+})
---
+++ vue-study/index.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+var Index = Vue.component('index', {
+    template: '<h1>Index</h1>'
+})
