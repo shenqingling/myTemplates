b5008dfb9dd8e9d68ae15a426e5fcdd962f05a9c
shenqingling <sql370@qq.com>
2017-06-29 23:06:31 +0800

    Vue CSR init

---
+++ vue-study/00CSR/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{greeting}} World!</p>
+        <ul>
+            <li v-for="m in list">
+                {{m.title}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+module.exports = {
+    // name: 'index',
+    data: function() {
+        return {
+            greeting: 'Hello',
+            list: []
+        }
+    },
+    mounted: function() {
+        $.get('https://www.easy-mock.com/mock/595508da9adc231f356dd755/movies-recommand/movies/list', (data) => {
+            this.list = data.entity
+        })
+    }
+}
+</script>
--- vue-study/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import './index.js';
-// import { Index } from './Index.vue';
-
-var app = new Vue({
-    el: '#app',
-    template: '<index />',
-// component: Index
-})
