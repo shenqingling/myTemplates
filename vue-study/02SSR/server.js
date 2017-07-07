b676188b7651f75d6c38dbd7436b9e41dd00ec41
沈庆玲 <sql370@qq.com>
2017-07-07 17:09:23 +0800

    合并server方法

--- vue-study/02SSR/server.js
+++ vue-study/02SSR/server.js
@@ -1,59 +1,56 @@
 const fs = require('fs')
 const path = require('path')
 const server = require('express')()
-const { createBundleRenderer } = require('vue-server-renderer')
+const {createBundleRenderer} = require('vue-server-renderer')
 
 // server.get('*', (req, res) => {
-  // const filePath = path.join(__dirname,'./dist/bundle.server.js')
-  // const file = fs.readFileSync(filePath, 'utf8')
-  // const renderer = createBundleRenderer(file)
-  // const context = { url: req.url }
-  // renderer.renderToString(context, (err, html) => {
-  //   if (err) {
-  //     console.log(err)
-  //     res.status(500).end('Internal Server Error')
-  //     return
-  //   }
-  //   console.log(html)
-  //   res.end(html)
-  // })
+// const filePath = path.join(__dirname,'./dist/bundle.server.js')
+// const file = fs.readFileSync(filePath, 'utf8')
+// const renderer = createBundleRenderer(file)
+// const context = { url: req.url }
+// renderer.renderToString(context, (err, html) => {
+//   if (err) {
+//     console.log(err)
+//     res.status(500).end('Internal Server Error')
+//     return
+//   }
+//   console.log(html)
+//   res.end(html)
+// })
 // })
 
 server.get('/:id', (req, res) => {
-    const filePath = path.join(__dirname,'./dist/bundle.server.js')
-    const file = fs.readFileSync(filePath, 'utf8')
-    const renderer = createBundleRenderer(file)
-    console.log(req.params.id)
-    const context = { url: req.params.id }
-    renderer.renderToString(context, (err, html) => {
-        if (err) {
-            console.log(err)
-            res.status(500).end('Internal Server Error')
-            return
-        }
-        console.log(html)
-        res.end(html)
-    })
+    const context = {
+        url: req.params.id
+    }
+    htmlRender(context, res)
 })
 
 server.get('/', (req, res) => {
-    const filePath = path.join(__dirname,'./dist/bundle.server.js')
+    const context = {
+        url: req.url
+    }
+    htmlRender(context, res)
+})
+
+function htmlRender(context, res) {
+    const filePath = path.join(__dirname, './dist/bundle.server.js')
     const file = fs.readFileSync(filePath, 'utf8')
     const renderer = createBundleRenderer(file)
-    const context = { url: req.url }
     renderer.renderToString(context, (err, html) => {
         if (err) {
             console.log(err)
             res.status(500).end('Internal Server Error')
             return
         }
-        console.log(html)
+        // console.log(html)
+        res.writeHead(200, {
+            'Content-Type': 'text/html;charset=utf-8'
+        });
         res.end(html)
     })
-})
+}
 
-
-
-server.listen(8090, function(){
-  console.log('server started on port 8090')
+server.listen(8090, function() {
+    console.log('server started on port 8090')
 })
